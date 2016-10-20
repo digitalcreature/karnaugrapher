fa9df36b429826835ba0cefabee29a9be77a429b
Tanner Grehawick <tgrehawi@gmail.com>
2016-10-19 22:11:11 -0400

    t -> tt

--- truthtable.lua
+++ truthtable.lua
@@ -2,34 +2,34 @@ truthtable = {}
 
 function truthtable.load(file)
 	local lines = file:lines()
-	local t = {
+	local tt = {
 		inputs = {add = table.insert},
 		outputs = {add = table.insert},
 	}
 	for c in lines():gmatch("(%w)") do
-		t.inputs:add(c)
+		tt.inputs:add(c)
 	end
 	for c in lines():gmatch("(%w)") do
-		t.outputs:add(c)
+		tt.outputs:add(c)
 	end
-	t.rowcount = math.pow(2, #t.inputs)
-	for r = 0, t.rowcount - 1 do
-		local line = lines() or string.rep("0", #t.outputs)
+	tt.rowcount = math.pow(2, #tt.inputs)
+	for r = 0, tt.rowcount - 1 do
+		local line = lines() or string.rep("0", #tt.outputs)
 		local row = {}
 		local n = r
-		for i, input in ipairs(t.inputs) do
-			i = #t.inputs - i
+		for i, input in ipairs(tt.inputs) do
+			i = #tt.inputs - i
 			local pow = math.pow(2, i + 1)
 		end
-		for o, output in ipairs(t.outputs) do
+		for o, output in ipairs(tt.outputs) do
 			row[output] = not line:sub(o, o):match("[ 0]")
 		end
-		t[r] = row
+		tt[r] = row
 	end
-	return t
+	return tt
 end
 
-function truthtable.display(t, s)
+function truthtable.display(tt, s)
 	local symbols = {
 		[false] = "0",
 		[true] = "1",
@@ -41,33 +41,33 @@ function truthtable.display(t, s)
 		symbols[k] = s[k] or symbols[k]
 	end
 	s = symbols
-	for _, c in ipairs(t.inputs) do
+	for _, c in ipairs(tt.inputs) do
 		io.write(" "..c.." ")
 	end
 	io.write(tostring	(s.vdiv))
-	for _, c in ipairs(t.outputs) do
+	for _, c in ipairs(tt.outputs) do
 		io.write(" "..c.." ")
 	end
 	print()
 	print(
-		string.rep(tostring(s.hdiv), #t.inputs)
+		string.rep(tostring(s.hdiv), #tt.inputs)
 		..tostring(s.cdiv)
-		..string.rep(tostring(s.hdiv), #t.outputs)
+		..string.rep(tostring(s.hdiv), #tt.outputs)
 	)
-	t.rowcount = math.pow(2, #t.inputs)
-	for r = 0, t.rowcount - 1 do
+	tt.rowcount = math.pow(2, #tt.inputs)
+	for r = 0, tt.rowcount - 1 do
 		local n = r
-		for i, input in ipairs(t.inputs) do
-			i = #t.inputs - i
+		for i, input in ipairs(tt.inputs) do
+			i = #tt.inputs - i
 			local pow = math.pow(2, i + 1)
 			io.write(" "..tostring(s[(n % pow) >= (pow / 2)]).." ")
 		end
 		io.write(tostring(s.vdiv))
-		for o, output in ipairs(t.outputs) do
-			local row = t[r]
+		for o, output in ipairs(tt.outputs) do
+			local row = tt[r]
 			io.write(" "..tostring(s[row and row[output]]).." ")
 		end
 		print()
-		t[r] = row
+		tt[r] = row
 	end
 end
